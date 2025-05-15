7de90371c07259035ee25c2948be54ed93211541
Tharles Cunha <cunhatharles@gmail.com>
2025-05-15 11:23:58 -0300

    Adiciona domínio Azure aos ALLOWED_HOSTS

--- servidor_tunel/settings.py
+++ servidor_tunel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&g#-sgyr)gc&vnh-$p*-ads+t#xiv0np*(qeronn81njy2-kz3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
